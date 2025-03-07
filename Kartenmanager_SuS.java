514b9c05fb1827427dfc087d8e598a9b7f922944
Janjoval <jjv.franke@gmail.com>
2025-03-07 13:23:09 +0100

    Bubble Sort hinzugefügt

--- Kartenmanager_SuS.java
+++ Kartenmanager_SuS.java
@@ -198,6 +198,74 @@ public class Kartenmanager_SuS extends Ereignisanwendung {
     // Ereignismethoden
     // ----------------------------------------------------------
 
+    public void Bubble_Sort
+      loescheAnzeige();
+        startZeit = System.nanoTime();
+
+        // z.B. 1000 Durchläufe
+        for(int I=0; I<1000; I++){
+            boolean sortiert = true;
+            karten = new Liste<Karte>(); 
+            initialisiereKarten(0);
+            do
+            {
+                sortiert = true;
+                for(int i = 1; i < karten.laenge(); i++)
+                {
+                    karten.geheZuPosition(i);
+                    Karte Karte_a = karten.aktuellesElement();           
+                    karten.geheZuPosition(i+1);
+                    Karte Karte_b = karten.aktuellesElement();
+                    if(Karte_a.wert > Karte_b.wert||Karte_a.wert == Karte_b.wert && Karte_a.farbe < Karte_b.farbe)
+                    {
+                        karten.ersetzeAktuelles(Karte_a);
+                        karten.geheZuPosition(i);
+                        karten.ersetzeAktuelles(Karte_b);
+                        sortiert = false;
+                    }
+                }
+
+            }while(!sortiert);
+
+        }
+
+        endZeit=System.nanoTime();
+        double d = endZeit - startZeit;
+        d = d/1000000;
+
+        zeichneKarten(0, 50, 150);
+        infoEtikett.setzeInhalt("Erfolgreich Sortiert. Zeit: "+ d + " ms");
+    }
+
+
+    public void Update_Klick() {
+        String s = tfUmfang.inhaltAlsText().trim();
+        int neuAnz;
+        try {
+            neuAnz = Integer.parseInt(s);
+            if(neuAnz<=0) neuAnz=52;
+        } catch(NumberFormatException ex) {
+            neuAnz=52;
+        }
+        loescheAnzeige();
+
+        // Alte Liste verwerfen => Neue Liste
+        karten = new Liste<Karte>();
+        kartenAnzahl= neuAnz;
+
+        initialisiereKarten(0);
+        zeichneKarten(0, 50, 150);
+
+        infoEtikett.setzeInhalt("Neuer Stapel: "+kartenAnzahl+" Karten (Liste).");
+    }
+
+    
+    
+    
+    
+    
+    
+    
     public void Sort_Klick() {
     loescheAnzeige();
     startZeit = System.nanoTime();
